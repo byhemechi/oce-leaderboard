40b5e9c1fdc6227abeff361607e4594962d0bc11
George Fischer <me@george.id.au>
2022-06-08 17:36:04 +1000

    Add last update route

--- http/update-leaderboards.ts
+++ http/update-leaderboards.ts
@@ -1,8 +1,16 @@
-import ms from "https://esm.sh/ms";
+import ms from "https://esm.sh/ms@2.1.3";
 let lastUpdated = 0;
 import updatePlayers from "../lib/update-players.ts";
+import { json } from "https://deno.land/x/sift@0.5.0/mod.ts";
 
 const updateFrequency = ms("1m");
+
+export const lastUpdatedRoute = () =>
+  json({
+    absolute: lastUpdated,
+    relative: Date.now() - lastUpdated,
+    human: ms(Date.now() - lastUpdated, { long: true }) + " ago",
+  });
 
 const updateLeaderboardRoute = async () => {
   const now = Date.now();
--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-import updateLeaderboardRoute from "./http/update-leaderboards.ts";
+import updateLeaderboardRoute, {
+  lastUpdatedRoute,
+} from "./http/update-leaderboards.ts";
 import tablePage from "./http/rankings.tsx";
 import { serve, serveStatic } from "https://deno.land/x/sift@0.5.0/mod.ts";
 
 serve({
   "/": tablePage,
   "/update_leaderboards": updateLeaderboardRoute,
+  "/last_updated": lastUpdatedRoute,
   "/:filename+": serveStatic("./static/", { baseUrl: import.meta.url }),
 });
