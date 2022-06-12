c9668b962f3fbcc7c3e0721625e832e7d777d585
George Fischer <me@george.id.au>
2022-06-12 17:14:21 +1000

    update database properly

--- lib/update-players.ts
+++ lib/update-players.ts
@@ -18,31 +18,28 @@ export default async function updatePlayers() {
       )
       .filter((i) => i !== null) ?? [];
 
-  await supabase.from("player").upsert([
-    ...(
-      await Promise.all(
-        [
-          ...extraPlayers.map(async (d) => {
-            if (d === null) return null;
-            const { player, country } = d;
-            const playerData = await getPlayer(player?.toString() ?? "");
-            if (playerData === null) return null;
-            playerData.country = country;
-            log.push(`Updated player ${player}`);
-            return playerData;
-          }),
-          new Array(pages).fill(null).map(async (_i, page) => {
-            const pageData = await getLeaderboard(
-              countries?.map((i) => i.name) ?? [],
-              page + 1
-            );
-            if (pageData == null) return null;
-            log.push(`Updated page ${page + 1}`);
-            return pageData.players;
-          }),
-        ].flat(1)
-      )
-    ).map((i) => ({ data: i })),
+  const updated = await Promise.all([
+    ...extraPlayers.map(async (d) => {
+      if (d === null) return null;
+      const { player, country } = d;
+      const playerData = await getPlayer(player?.toString() ?? "");
+      if (playerData === null) return null;
+      playerData.country = country;
+      log.push(`Updated player ${player}`);
+      return playerData;
+    }),
+    ...new Array(1).fill(null).map(async (_i, page) => {
+      const pageData = await getLeaderboard(
+        countries?.map((i) => i.name) ?? [],
+        page + 1
+      );
+      if (pageData == null) return null;
+      log.push(`Updated page ${page + 1}`);
+      return pageData.players;
+    }),
   ]);
+  await supabase
+    .from("player")
+    .upsert([...updated.flat(Infinity).map((i) => ({ data: i }))]);
   return log;
 }
